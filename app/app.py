acb084d212b0a0f9396f3d7cf0f6f4b631257e3d
willardshikami <willardshikami@gmail.com>
2016-08-17 15:30:38 +0300

    started the web application

--- app/app.py
+++ app/app.py
@@ -2,5 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hi'
+def index():
+    return '<h1>Flask Project</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
